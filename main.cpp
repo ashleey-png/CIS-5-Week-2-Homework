225218e07c03c183dab6ed98f48308eae6dcec34
Ashley Duran <ashleedur@gmail.com>
2026-09-06 23:20:44 -0700

    Complete Week 2 Homework

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
-// Practice 2 — Your Name
+// Practice 2 — Ashley Duran
 // CIS 5 Week 02 · Structure polish
 
 int main() {
   // Block 1 — who you are (from Lab 2)
   std::cout << "=== About me ===\n";
-  // TODO: 3+ labeled lines
+  std::cout << "Name: Ashley Duran\n";
+  std::cout << "Interests: video games\n"; 
+  std::cout << "Tool path: VS Code\n";
+  std::cout << "Fun fact: I'm teaching myself how to animate\n";
+
 
   // Block 2 — this week's goals (from outcomes)
   std::cout << "=== Week 02 goals ===\n";
-  // TODO: list 3 goals as complete sentences
-
+  std::cout << "I'll be able to build, run, and correct simple errors in my code.\n";
+  std::cout << "I'll practice the basics of forking, cloning, commiting, and pushing files through VS Code and Github.\n"; 
+  std::cout << "I'll become more comfortable navigating through VS Code without having to look at tutorials constantly.\n";
   return 0;
 }
